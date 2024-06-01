d2a6dd5273c8ef63c540a7c85b96a82206d19256
Frank Okezie Obiedere <obiedereokezie@gail.com>
2024-06-01 07:26:49 +0000

    refactor

---
+++ src/app/express/index.ts
@@ -0,0 +1,19 @@
+import express, {
+  json,
+  urlencoded,
+  Request,
+  Response,
+  NextFunction,
+} from "express";
+import logger from "morgan";
+import cors from "cors";
+
+export const app = express();
+
+app.use(cors());
+app.use(logger("dev"));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+
+// const dev = "http://localhost:5000/api/v1";
+// const auth = `Bearer token`
